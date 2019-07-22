fd79490232adbfdd4cdde01ae54884b3d3d326ac
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2019-07-21 19:08:31 -0700

    Remove unnecessary building steps

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,10 @@
-const {src, dest, parallel} = require('gulp');
+const {src, dest} = require('gulp');
 const sass = require('gulp-sass');
-const clean_ = require('gulp-clean');
 
-function html() {
-  return src(['sample.html'])
-    .pipe(dest('output/'));
+function make_sass() {
+  return src('*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(dest('.'));
 }
 
-function css() {
-  return src(['style.scss'])
-    .pipe(sass().on('error', sass.logError))
-    .pipe(dest('output/'));
-}
-
-function js() {
-  return src(['sample.js'])
-    .pipe(dest('output/'));
-}
-
-function clean() {
-  return src('output/*')
-    .pipe(clean_());
-}
-
-exports.default = parallel(html, css, js);
-exports.clean = clean;
-
+exports.default = make_sass;
