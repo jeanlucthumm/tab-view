48dbdcbd5c8fb027311062b5077220bfc1d4a161
Jean-Luc Thumm <jthumm@ucsd.edu>
2019-08-17 17:50:51 -0700

    Refactor content script

--- content.js
+++ content.js
@@ -13,20 +13,18 @@ function setup() {
   style.href = chrome.extension.getURL('content.css');
   (document.head || document.documentElement).appendChild(style);
 
+  window.addEventListener('message', onMessage, false);
+
   // Inject the modal
   let wrapper = $('<div>');
   wrapper.attr('id', 'tab-view-content-wrapper');
   wrapper.load(chrome.extension.getURL('modal.html'), () => {
     wrapper.find('#tab-view-iframe')
       .attr('src', chrome.runtime.getURL('popup.html'));
+    wrapper.appendTo($(document.body));
 
-    // TODO the wrapper may not be on the DOM when this runs
     setupModal();
   });
-  wrapper.appendTo($(document.body));
-
-  // Register event listeners
-  window.addEventListener('message', onMessage, false);
 }
 
 // Registers event listeners and does some CSS magic after the modal
